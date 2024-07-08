cc613a791f4b426330d8d06486d370580f6a3f74
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-07-08 15:29:46 +0300

    Add createdAt and updatedAt properties to the users being created

--- client/src/components/user-section/UserSection.jsx
+++ client/src/components/user-section/UserSection.jsx
@@ -37,7 +37,11 @@ export default function UserSection(props){
       
       const formData = new FormData(e.currentTarget)
 
-      const userData = Object.fromEntries(formData);
+      const userData ={ 
+        ...Object.fromEntries(formData),
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      };
 
       const response = await fetch(`${baseUrl}/users`,{
           method: 'POST',
